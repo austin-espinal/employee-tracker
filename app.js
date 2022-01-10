b507edb02aa333ddf82c6089322702c23b091d2d
austin espinal <austinespi@gmail.com>
2022-01-09 19:09:49 -0500

    updated role and employee modules. made mainMenu into a module. working on issue with switch

--- app.js
+++ app.js
@@ -1,85 +1,17 @@
 //imports database 
 const db = require('./db/connection');
-//inquirer package
-const inquirer = require('inquirer');
 
-//department module
-const {viewAllDepartments, addDepartment} = require('./lib/department');
-//role module
-const {viewAllRoles} = require('./lib/role');
-//employee module
-const {viewAllEmployees} = require('./lib/employee');
+//import mainMenu function
+const mainMenu = require('./lib/menu');
 
-//notifies user that database is connected or not
+//notifies user that database is connected or not and starts app if database is connected
 db.connect(err => {
     if (err) throw err;
     console.log('Database connected.');
+    console.log(`==================
+
+                    Employee Tracker
+
+                ===================`);
+    mainMenu();
 });
-
-//function to exit app and database
-const Exit = () => {
-    console.log('Goodbye!');
-    console.log('Database connection ended');
-    db.end();
-}
-
-//main menu that user will see and select from
-const mainMenu = () => {
-    inquirer.prompt([
-        {
-            type: 'list',
-            name: 'choices',
-            message: 'Please choose one an option from this list',
-            choices: [
-                'View all departments',
-                'View all roles',
-                'View all employees',
-                'Add a department',
-                'Add a role',
-                'Add an employee',
-                'Update an employee role',
-                'Exit'
-                ]
-        }
-    ])
-    .then(({choice}) => {
-        switch(choice) {
-            case 'View all departments':
-                //view all depart function
-                viewAllDepartments();
-                break;
-
-            case 'View all roles':
-                //view all roles function
-                viewAllRoles();
-                break;
-
-            case 'View all employees':
-                //view all employees function
-                viewAllEmployees();
-                break;
-
-            case 'Add a department':
-                //add depart function
-                addDepartment();
-                break;
-
-            case 'Add a role':
-                //insert add role function
-                break;
-
-            case 'Add an employee':
-                //insert add employee function
-                break;
-    
-            case 'Update an employee role':
-                //insert update employee function
-                break;
-    
-            default:
-                Exit();
-        }
-    })
-};
-
-module.exports = {mainMenu};
